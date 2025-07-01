5d0855ce7331cff0bf6556dca973f6aa6f7eaea1
Marcos Lohmann <marcos.lohmann@gmail.com>
2025-07-01 16:03:56 -0300

    Create open_ports.sh

---
+++ docker/open_ports.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo iptables -I INPUT -p tcp --dport 5678 -j ACCEPT
+sudo iptables -I INPUT -p tcp --dport 1880 -j ACCEPT
+sudo iptables -I INPUT -p tcp --dport 8000 -j ACCEPT
+sudo iptables -I INPUT -p tcp --dport 80 -j ACCEPT
+sudo iptables -I INPUT -p tcp --dport 443 -j ACCEPT
+sudo netfilter-persistent save
