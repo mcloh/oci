1c249e7eaf49e7eff72fe05affa41c94e4449dce
Marcos Lohmann <marcos.lohmann@gmail.com>
2025-09-09 13:28:50 -0300

    Update open_ports.sh

--- docker/open_ports.sh
+++ docker/open_ports.sh
@@ -3,6 +3,7 @@
 sudo iptables -I INPUT -p tcp --dport 5678 -j ACCEPT
 sudo iptables -I INPUT -p tcp --dport 1880 -j ACCEPT
 sudo iptables -I INPUT -p tcp --dport 8000 -j ACCEPT
+sudo iptables -I INPUT -p tcp --dport 8080 -j ACCEPT
 sudo iptables -I INPUT -p tcp --dport 80 -j ACCEPT
 sudo iptables -I INPUT -p tcp --dport 443 -j ACCEPT
 sudo netfilter-persistent save
